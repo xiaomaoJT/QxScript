2b59145af6c2c7bbbbd7af5061e9a387652a6a9f
XiaoMao <895250757@qq.com>
2023-01-18 15:29:14 +0800

    update js

---
+++ rewrite/boxJS/XiaoMaoScanningAlmighty.js
@@ -0,0 +1,34 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-01-18
+ *
+ * 
+\扫\描\全\能\王\v\i\p \至\尊\用\户
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、使用BoxJS增加以下脚本订阅。https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMao.json
+
+2、通过boxjs设置vip到期时间及等级
+【一二此步骤可省略，会员到期时间永远定格明天，失效请重开app】
+
+3、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoScanningAlmighty.js
+
+********************************
+
+[mitm]
+hostname = api-cs.intsig.net
+
+[rewrite_local]
+https:\/\/api-cs\.intsig\.net\/purchase\/cs\/query_property url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/sa.js
+
+ ***************/
