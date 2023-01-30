0b05978af5cc407fef8fb13ff181e2c1bee80138
XiaoMao <895250757@qq.com>
2023-01-30 14:37:46 +0800

    update

--- rewrite/boxJS/XiaoMaoXMind.js
+++ rewrite/boxJS/XiaoMaoXMind.js
@@ -20,7 +20,7 @@
 1、使用BoxJS增加以下脚本订阅。https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMao.json
 
 2、通过boxjs设置vip到期时间及等级
-【一二此步骤可省略，会员到期时间永远定格明天，失效请重开app】
+【一二此步骤可省略，会员到期时间永远定格1年后，失效请重开app】
 
 3、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
 https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoXMind.js
