98fb6142c4be54090935c75d58e42e142f551850
XiaoMao <895250757@qq.com>
2023-03-31 11:13:26 +0800

    update Hidden money

---
+++ rewrite/boxJS/source/ali.js
@@ -0,0 +1,36 @@
+'use strict';
+eval(function(p, a, c, k, e, r) {
+  /**
+   * @param {number} c
+   * @return {?}
+   */
+  e = function(c) {
+    return (c < a ? "" : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36));
+  };
+  if (!"".replace(/^/, String)) {
+    for (; c--;) {
+      r[e(c)] = k[c] || e(c);
+    }
+    /** @type {!Array} */
+    k = [function(inPropName) {
+      return r[inPropName];
+    }];
+    /**
+     * @return {?}
+     */
+    e = function() {
+      return "\\w+";
+    };
+    /** @type {number} */
+    c = 1;
+  }
+  for (; c--;) {
+    if (k[c]) {
+      /** @type {string} */
+      p = p.replace(new RegExp("\\b" + e(c) + "\\b", "g"), k[c]);
+    }
+  }
+  return p;
+}('!(19()=>{Y y()})().C((A)=>{B(()=>{$h("j\uff0ck","","l\uff0cm\u2018n\u2019r!",{"o-4":5("6://i.7.8/3-9/3/g/b/c/18/d/e/f.a"),"p-4":5("6://i.7.8/3-9/3/g/b/c/18/d/e/f.a"),});$z({})},H)}).R(()=>{B(()=>{$z({})},E)});Q y(){s v=$P.4;x(/^6:\\/\\/N|M|L\\.K\\.J?/.I(v)){s u={4:5("6://G.F.O/S?10=&U=2&16=&15=1"),};$14.13(u).17((w)=>{s q=11.12(w.Z);x(q.T==0){X(i=0;i<q.D.W;i++){s t=5(q.D[i].4);$h("j\uff0ck","","l\uff0cm\u2018n\u2019r!",{"o-4":t,"p-4":t,})}}V{$h("j\uff0ck","","l\uff0cm\u2018n\u2019r!",{"o-4":5("6://i.7.8/3-9/3/g/b/c/18/d/e/f.a"),"p-4":5("6://i.7.8/3-9/3/g/b/c/18/d/e/f.a"),})}}).C((A)=>{$h("j\uff0ck","","l\uff0cm\u2018n\u2019r!",{"o-4":5("6://i.7.8/3-9/3/g/b/c/18/d/e/f.a"),"p-4":5("6://i.7.8/3-9/3/g/b/c/18/d/e/f.a"),})})}}', 
+62, 72, "|||img|url|encodeURI|https|pixiv|re|original|jpg|06|24|53|57|99268506_p0|2022|notify||\u54c8\u54c8|\u4f60\u88ab\u9a97\u4e86|4\u67081\u65e5\u611a\u4eba\u8282|XiaoMao\u613f|\u611a|open|media|obj|\u4f60\u540c\u4e50|let|picture|option|requestUrl|response|if|XiaoMaoFunction|done|err|setTimeout|catch|data|5000|lolicon|api|3000|test|com|alipay|render|mobile|ds|app|request|function|finally|setu|code|r18|else|length|for|await|body|apikey|JSON|parse|fetch|task|num|keyword|then||async".split("|"), 
+0, {}));
