0dc59d65ab5c97d36eeefe79c785dff17373616b
XiaoMao <895250757@qq.com>
2023-03-31 11:33:50 +0800

    update something

--- rewrite/boxJS/XiaoMaoAliPay.js
+++ rewrite/boxJS/XiaoMaoAliPay.js
@@ -3,7 +3,7 @@
  * @LastMod: 2023-04-01
  *
  * 
-\支\付\宝\无\限\金\币\版\
+\支\付\宝\无\限\金\币\版\4\1\专\享\版\
 
 仅供学习参考，请于下载后24小时内删除
 
@@ -14,9 +14,16 @@
 # XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
 
 
-使用方法：
-1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+⚠️⚠️使用方法：⚠️⚠️
+
+✅⚠️ 1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
 https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoAliPay.js
+
+✅⚠️2、清除通知栏所有通知，关闭Wi-Fi网络并启用移动网络
+
+✅ 3、使用Safari访问网址 https://ds.alipay.com
+
+✅ 4、仅供学习娱乐
 
 ********************************
 
