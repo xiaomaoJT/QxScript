b46c20c13b36d71fb058f2382bffa7a56e79a346
XiaoMao <895250757@qq.com>
2023-04-05 17:38:48 +0800

    update js

---
+++ rewrite/boxJS/XiaoMaoTodayHotList.js
@@ -0,0 +1,37 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-04-05
+ *
+ * 
+
+
+\今\日\热\搜\v\i\p\自\定\义\
+
+官方会员兑换码：0B4KM
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、使用BoxJS增加以下脚本订阅。https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMao.json
+
+2、通过boxjs设置vip到期时间及等级
+【一二此步骤可省略，会员到期时间永远定格1明天，失效请重开app】
+
+3、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoTodayHotList.js
+
+********************************
+
+[mitm]
+hostname = api2.tophub.app
+
+[rewrite_local]
+https:\/\/api2\.tophub\.app\/account\/sync url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/hb.js
+ ***************/
