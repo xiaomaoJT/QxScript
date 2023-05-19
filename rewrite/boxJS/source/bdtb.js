ef2c347fede19edddab2bdd94fc8eee18bfa43ef
XiaoMao <49260609+xiaomaoJT@users.noreply.github.com>
2023-05-19 21:13:24 +0800

    Update bdtb.js

--- rewrite/boxJS/source/bdtb.js
+++ rewrite/boxJS/source/bdtb.js
@@ -1,7 +1,7 @@
 let requestUrl = $request.url;
 
 if (/^https?:\/\/wapp\.baidu\.com\/p\/[0-9]{3,}\?query/.test(requestUrl)) {
-  let url = requestUrl.substring(0, requestUrl.indexOf("?"));
+  let url = requestUrl.substring(0, requestUrl.indexOf("?")).replace('wapp','tieba');
 
   $notify("XiaoMao提示", "", "捕捉到贴吧源地址，点击跳转!", {
     "open-url": url,
