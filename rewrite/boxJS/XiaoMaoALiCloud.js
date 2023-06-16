32679a1b7c5d72806b2d65a30ab908d701e9b182
XiaoMao <895250757@qq.com>
2023-06-16 10:00:08 +0800

    update alic

--- rewrite/boxJS/XiaoMaoALiCloud.js
+++ rewrite/boxJS/XiaoMaoALiCloud.js
@@ -1,6 +1,6 @@
 /**************************
  *  * @Author: XiaoMao
- * @LastMod: 2023-06-15
+ * @LastMod: 2023-06-16
  *
  * 
 
@@ -37,5 +37,7 @@ https:\/\/member\.aliyundrive\.com\/v1\/users\/tools url script-response-body ht
 https:\/\/api\.aliyundrive\.com\/business\/v1\/users\/me\/vip\/info url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
 https:\/\/api\.aliyundrive\.com\/apps\/v1\/users\/home\/widgets url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
 https://api.aliyundrive.com/apps/v1/users/home/news url reject-200
-https:\/\/api\.aliyundrive\.com\/adrive\/v1\/app\/logos url  script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
+https:\/\/api\.aliyundrive\.com\/adrive\/v1\/app\/logos url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
+https:\/\/api\.aliyundrive\.com\/adrive\/v2\/user\/get url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
+https:\/\/api\.aliyundrive\.com\/business\/v1.0\/users\/feature\/list url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
  ***************/
