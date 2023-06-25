f39f24f470f27a035b3eca2a6b4f9399a6f9889d
XiaoMao <895250757@qq.com>
2023-06-25 17:33:55 +0800

    add nymfJS

---
+++ rewrite/boxJS/XiaoMaoNYMF.js
@@ -0,0 +1,34 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-06-25
+ *
+
+
+\N\Y\M\F\ \P\r\o\图\片\ \视\频\ \故\事\ \模\特\资\源\解\锁\ \去\除\马\赛\克\
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、应用下载地址：https://apps.apple.com/us/app/nymf-%E8%89%BA%E6%9C%AF%E9%A1%B9%E7%9B%AE%E8%A3%B8%E4%BD%93%E6%91%84%E5%BD%B1/id1495951015?l=zh
+
+2、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoNYMF.js
+
+********************************
+
+[mitm]
+hostname = nymfapp.com
+
+[rewrite_local]
+https:\/\/nymfapp\.com\/api\/(photos|posts|stories|compose) url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/nymf.js
+
+ ***************/
+
+
+
+
