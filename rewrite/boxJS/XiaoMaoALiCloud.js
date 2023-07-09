235745960b9befc934327b7cb47f84916bd96d08
XiaoMao <895250757@qq.com>
2023-07-09 14:19:16 +0800

    update alic

--- rewrite/boxJS/XiaoMaoALiCloud.js
+++ rewrite/boxJS/XiaoMaoALiCloud.js
@@ -1,10 +1,11 @@
 /**************************
  *  * @Author: XiaoMao
- * @LastMod: 2023-07-07
+ * @LastMod: 2023-07-09
  *
  * 
 
 \阿\里\云\盘\s\v\i\p\+\部\分\优\化\+\去\广\告\+\彩\蛋\+\自\定\义\+\解\锁\所\有\图\标\
+\解\锁\倍\率\、\长\按\倍\率\、\清\晰\度\等\
 
 仅供学习参考，请于下载后24小时内删除
 
@@ -20,6 +21,9 @@
 
 2、通过boxjs设置svip到期时间及等级
 【一二此步骤可省略，会员到期时间永远定格明天，失效请重开app】
+【支持更改云容量显示，默认不更改】
+【支持更改图标布局】
+【支持获取视频m3u8临时下载链接，默认关闭】
 
 3、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
 https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoALiCloud.js
@@ -41,4 +45,5 @@ https:\/\/api\.aliyundrive\.com\/adrive\/v1\/app\/logos url script-response-body
 https:\/\/api\.aliyundrive\.com\/adrive\/v2\/user\/get url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
 https:\/\/api\.aliyundrive\.com\/business\/v1.0\/users\/feature\/list url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
 https:\/\/api\.aliyundrive\.com\/business\/v1.0\/users\/feature\/trial url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
+https:\/\/api\.aliyundrive\.com\/v2\/file\/get_video_preview_play_info url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/alic.js
  ***************/
