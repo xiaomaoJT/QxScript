e65fa3a1a4872531633b6ad7e2771b61c7e4bbba
XiaoMao <895250757@qq.com>
2023-08-02 09:01:44 +0800

    add js

---
+++ rewrite/script/js/cyw_token_get.js
@@ -0,0 +1,58 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-08-02
+ *
+ * 
+
+\彩\云\天\气\个\人\t\o\k\e\n\捕\获\
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、清楚彩云天气缓存，重新登录
+
+2、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器 > ⚠️打开插入资源
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/script/js/cyw_token_get.js
+
+3、搭配token可实现会员多端共享 （教程地址：https://t.me/xiaomaoJT/710）
+
+********************************
+
+[mitm]
+hostname = biz.caiyunapp.com,biz.cyapi.cn
+
+[rewrite_local]
+https?:\/\/biz\.(caiyunapp|cyapi)\.(com|cn)\/(membership_rights|v2\/user) url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/script/js/cyw_token_get.js
+
+
+ ***************/
+
+var obj = JSON.parse($response.body);
+let requestUrl = $request.url;
+if (
+  /^https?:\/\/biz\.(caiyunapp|cyapi)\.(com|cn)\/(membership_rights|v2\/user)?/.test(
+    requestUrl
+  )
+) {
+  if (obj.result.hasOwnProperty("token")) {
+    $notify(
+      "XiaoMao提示",
+      "彩云天气个人Token获取成功",
+      "个人Token为:" + "\n" + obj.result.token
+    );
+  } else {
+    $notify(
+      "XiaoMao提示",
+      "彩云天气个人Token获取失败",
+      "请清除彩云天气缓存，退出登录后重新登录再试！"
+    );
+  }
+}
+
+$done({});
