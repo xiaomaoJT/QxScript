0b4f5e0ea75c7ac87853ff574e0993a288d37dd7
XiaoMao <895250757@qq.com>
2023-08-11 10:26:20 +0800

    update js

--- rewrite/boxJS/XiaoMaoWeiBo.js
+++ rewrite/boxJS/XiaoMaoWeiBo.js
@@ -1,10 +1,10 @@
 /**************************
  *  * @Author: XiaoMao
- * @LastMod: 2023-07-14
+ * @LastMod: 2023-08-11
  *
  * 
 
-\微\博\
+\微\博\+\微\博\轻\享\版\/\国\际\版\
 \解\锁\全\部\会\员\图\标\、\限\时\图\标\
 \解\锁\部\分\个\性\小\尾\巴\
 
@@ -20,17 +20,20 @@
 1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
 https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoWeiBo.js
 
-2、解锁位置：微博 > 我 > 右上角设置 > 会员专属设置 > 个性图标/微博来源
+2、解锁位置：
+微博 > 我 > 右上角设置 > 会员专属设置 > 个性图标/微博来源
+微博轻享版 > 我 > 设置 > 通用 > 显示设置 > APP图标
 
 
 ********************************
 
 
 [mitm]
-hostname = new.vip.weibo.cn,api.weibo.cn
+hostname = new.vip.weibo.cn,api.weibo.cn,weibointl.api.weibo.cn
 
 [rewrite_local]
 https:\/\/new\.vip\.weibo\.cn\/aj\/appicon\/list url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/wb.js
 https:\/\/api\.weibo\.cn\/device\/get url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/wb.js
+https:\/\/weibointl\.api\.weibo\.cn\/portal\.php url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/wb.js
 
 ********************************/
