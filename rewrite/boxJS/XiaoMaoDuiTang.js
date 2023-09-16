ffbda7493934979f0f01b3f3e09f8423b6406148
XiaoMao <895250757@qq.com>
2023-09-17 00:21:54 +0800

    update js

--- rewrite/boxJS/XiaoMaoDuiTang.js
+++ rewrite/boxJS/XiaoMaoDuiTang.js
@@ -5,7 +5,7 @@
  * 
 
 
-\堆\糖\s\v\i\p\、\去\除\部\分\广\告\、\去\除\部\分\推\广\
+\堆\糖\s\v\i\p\、\去\除\部\分\广\告\、\去\除\部\分\推\广\去\开\屏\广\告\
 
 仅供学习参考，请于下载后24小时内删除
 
@@ -19,12 +19,12 @@
 使用方法：
 1、使用BoxJS增加以下脚本订阅。https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMao.json
 
-2、通过boxjs设置svip到期时间及等级
+2、通过boxjs设置svip到期时间及等级【不设置默认永久10天后】
 
 3、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
 https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoDuiTang.js
 
-4、去除开屏广告，下载安装App后，先打开QX抓包模式，成功登录账号后关闭，当前测试暂未出现开屏
+4、去除开屏广告，下载安装App后，先开启脚本，打开QX抓包模式，成功登录账号后再关闭，当前测试暂未出现开屏
 
 ********************************
 
@@ -43,7 +43,7 @@ http:\/\/api\.duitang\.com\/napi\/index\/discovery url script-response-body http
 # \数\据\流\广\告\
 http:\/\/api\.duitang\.com\/napi\/settings url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/dt.js
 # \s\v\i\p\
-http[s]:\/\/(www|api)\.duitang\.com\/napi\/people\/me url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/dt.js
+https?:\/\/(www|api)\.duitang\.com\/napi\/people\/me url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/dt.js
 https:\/\/www\.duitang\.com\/napi\/vienna\/graphic url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/dt.js
 
  ***************/
