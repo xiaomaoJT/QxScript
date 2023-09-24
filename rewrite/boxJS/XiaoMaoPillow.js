ab67a2b28a68827ba96020c726fcbb14b47b3a26
XiaoMao <895250757@qq.com>
2023-09-24 22:50:35 +0800

    no message

--- rewrite/boxJS/XiaoMaoPillow.js
+++ rewrite/boxJS/XiaoMaoPillow.js
@@ -32,7 +32,7 @@ https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoP
 hostname = api.revenuecat.com
 
 [rewrite_local]
-https:\/\/api\.revenuecat\.com\/v1\/(subscribers|receipts) url script-response-body pi.js
-https:\/\/api\.revenuecat\.com\/v1\/(subscribers|receipts) url script-response-header pi.js
+https:\/\/api\.revenuecat\.com\/v1\/(subscribers|receipts) url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/pi.js
+https:\/\/api\.revenuecat\.com\/v1\/(subscribers|receipts) url script-response-header https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/pi.js
 
 ********************************/
