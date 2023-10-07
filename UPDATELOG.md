5902ba750556da785aa831defa6a2c470d5d74c4
XiaoMao <895250757@qq.com>
2023-10-07 11:40:44 +0800

    ad bh

--- UPDATELOG.md
+++ UPDATELOG.md
@@ -66,9 +66,9 @@ resource_parser_url= https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/mast
 
 > 小帽语：日常更新如**节点订阅更新**、**去广告分流更新**、**去广告重写更新**将不推送更新提示及更新日志，请自行QX软件内开启对应资源的默认更新即可自动获取，最新更新时间以首页或对应资源内说明文字或本页的更新时间为准。
 >
-> 本仓自3.0版本后 于2022年12月1日起 已持续更新 ***410*** 次
+> 本仓自3.0版本后 于2022年12月1日起 已持续更新 ***411*** 次
 >
-> ***最新更新时间 2023.10.07 19:48***
+> ***最新更新时间 2023.10.07 11:48***
 
 
 + ##### 20231007
@@ -76,6 +76,8 @@ resource_parser_url= https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/mast
   * > 新增[md脚本](https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoMiiDii.js)
     >
     > 优化懒人配置内策略组美国策略正则，剔除rus、aus等错误匹配
+    >
+    > 新增[bh脚本](https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoBHPro.js)
     >
 
 
---
+++ rewrite/boxJS/XiaoMaoBHPro.js
@@ -0,0 +1,36 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-10-07
+ *
+ * 
+
+\B\H\ \P\r\o\ \解\锁\调\研\G\o\数\据\解\锁\会\员\
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoBHPro.js
+
+********************************
+
+[mitm]
+hostname = litebhapi.belugabh.com
+
+[rewrite_local]
+https:\/\/litebhapi\.belugabh\.com\/personal_center\/user_equity_status_list url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/bh.js
+https:\/\/litebhapi\.belugabh\.com\/\/momentsHome\/getClassifyList url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/bh.js
+https:\/\/litebhapi\.belugabh\.com\/personal_center\/user_info url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/bh.js
+https:\/\/litebhapi\.belugabh\.com\/personal_center\/my_homepage url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/bh.js
+https:\/\/litebhapi\.belugabh\.com\/market_survey_report\/periphery url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/bh.js
+https:\/\/litebhapi\.belugabh\.com\/home_page\/banner_image url reject
+
+ ***************/
+
