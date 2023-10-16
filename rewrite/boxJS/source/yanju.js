1b57d02039eda0fee94cc3488d38091579bd80f9
XiaoMao <895250757@qq.com>
2023-10-16 22:25:39 +0800

    update

--- rewrite/boxJS/source/yanju.js
+++ rewrite/boxJS/source/yanju.js
@@ -1,1 +1,13 @@
-let requestUrl=$request.url;if(/^http:\/\/.+.wscreativity\.com\/yanju\/.+\.(jpg|png)?/.test(requestUrl)){$notify("捕获到当前图片链接❗️","","点击跳转浏览器查看❗️",{"open-url":requestUrl,});$done(JSON.stringify($request))}var obj=JSON.parse($response.body.replace(/\"productType\":\w+/g,'"productType":1'));$done({body:JSON.stringify(obj)});
+let requestUrl = $request.url;
+if (/^http:\/\/.+.wscreativity\.com\/yanju\/.+\.(jpg|png)?/.test(requestUrl)) {
+  if ($request.headers["User-Agent"].indexOf("yanju") != -1) {
+    $notify("捕获到当前图片链接❗️", "", "点击跳转浏览器查看❗️", {
+      "open-url": requestUrl,
+    });
+  }
+  $done(JSON.stringify($request));
+}
+var obj = JSON.parse(
+  $response.body.replace(/\"productType\":\w+/g, '"productType":1')
+);
+$done({ body: JSON.stringify(obj) });
