67a97c788bd54bdfce89602e38ae2f0866307204
XiaoMao <895250757@qq.com>
2023-10-16 11:00:32 +0800

    add js

---
+++ rewrite/boxJS/XiaoMaoYanJu.js
@@ -0,0 +1,34 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-10-16
+ *
+ * 
+
+\言\橘\ \解\锁\A\D\内\容\ \增\加\图\片\链\接\捕\获\
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoYanJu.js
+
+2、链接捕获依赖于网络，若图片存在缓存将无法捕获，可尝试清理缓存后再试～
+
+
+********************************
+
+[mitm]
+hostname = *.wscreativity.com
+
+[rewrite_local]
+http:\/\/.+.wscreativity\.com\/yanju\/.+\.(jpg|png) url script-request-header yanju.js
+https:\/\/yanju63resapi\.wscreativity\.com url script-response-body yanju.js
+ ***************/
+
