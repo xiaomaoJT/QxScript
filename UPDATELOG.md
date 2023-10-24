cf43efa6e10ab873421c6cbc6ff62ea1b613fea7
XiaoMao <895250757@qq.com>
2023-10-24 23:22:56 +0800

    add efh

--- UPDATELOG.md
+++ UPDATELOG.md
@@ -66,7 +66,7 @@ resource_parser_url= https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/mast
 
 > 小帽语：日常更新如**节点订阅更新**、**去广告分流更新**、**去广告重写更新**将不推送更新提示及更新日志，请自行QX软件内开启对应资源的默认更新即可自动获取，最新更新时间以首页或对应资源内说明文字或本页的更新时间为准。
 >
-> 本仓自3.0版本后 于2022年12月1日起 已持续更新 ***435*** 次
+> 本仓自3.0版本后 于2022年12月1日起 已持续更新 ***436*** 次
 >
 > ***最新更新时间 2023.10.24 22:05***
 
@@ -79,6 +79,8 @@ resource_parser_url= https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/mast
     > 移动设备端中文版懒人规则更新，优化内置策略启用，新增XiaoMao脚本集
     >
     > 新增[hxcj脚本](https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoHeXun.js)
+    >
+    > 新增[efh脚本](https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoEFHello.js)
     >
 
 
---
+++ rewrite/boxJS/XiaoMaoEFHello.js
@@ -0,0 +1,36 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-10-24
+ *
+ * 
+
+
+\E\F\ \H\e\l\l\o\ \P\r\o\
+
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、使用BoxJS增加以下脚本订阅。https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMao.json
+
+2、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoEFHello.js
+
+3、「因Pro解锁数据无法保存」根据需要解锁的章节，通过BoxJS进行配置解锁。
+
+
+[mitm]
+hostname = api.hello.ef.com
+
+[rewrite_local]
+
+https:\/\/api\.hello\.ef\.com\/graphql url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/efh.js
+
+********************************/
