f1d70b14940ed3ef087c455c7f5861954451ecbc
XiaoMao <895250757@qq.com>
2023-10-25 11:50:21 +0800

    add js & update rw5

--- UPDATELOG.md
+++ UPDATELOG.md
@@ -66,7 +66,7 @@ resource_parser_url= https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/mast
 
 > 小帽语：日常更新如**节点订阅更新**、**去广告分流更新**、**去广告重写更新**将不推送更新提示及更新日志，请自行QX软件内开启对应资源的默认更新即可自动获取，最新更新时间以首页或对应资源内说明文字或本页的更新时间为准。
 >
-> 本仓自3.0版本后 于2022年12月1日起 已持续更新 ***438*** 次
+> 本仓自3.0版本后 于2022年12月1日起 已持续更新 ***439*** 次
 >
 > ***最新更新时间 2023.10.25 09:38***
 
@@ -77,6 +77,10 @@ resource_parser_url= https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/mast
 + ##### 20231025
 
   * > 新增[dr脚本](https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoDrops.js)
+    >
+    > 新增[mix脚本](https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoMix.js)
+    >
+    > 重写5 移除黄油脚本
     >
 
 + ##### 20231024
---
+++ rewrite/boxJS/XiaoMaoMix.js
@@ -0,0 +1,42 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-10-25
+ *
+ * 
+
+
+\M\i\x\ \V\I\P\年\度\会\员\+\净\化\
+
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoMix.js
+
+2、打开软件 > 右上角 > 恢复购买
+
+3、拦截Mix社区加载 host, mix-community.camera360.com, reject
+
+
+[mitm]
+hostname = mix-api.camera360.com,cdn-bm.camera360.com
+
+[rewrite_local]
+https:\/\/mix-api\.camera360\.com\/v1\/operational-positions url reject
+https:\/\/cdn-bm\.camera360\.com\/api\/mix\/recovery url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/mix.js
+https:\/\/cdn-bm\.camera360\.com\/api\/iap\/check-receipt url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/mix.js
+https:\/\/cdn-bm\.camera360\.com\/\/api\/mix-asset\/assets url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/mix.js
+
+
+
+********************************/
+
+
