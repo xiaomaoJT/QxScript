cfd63a80e7d77d350299f3fb16ad18521d347040
XiaoMao <895250757@qq.com>
2023-11-03 09:03:52 +0800

    add no

---
+++ rewrite/boxJS/XiaoMaoNoted.js
@@ -0,0 +1,37 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-11-03
+ *
+ * 
+
+
+\N\o\t\e\d\ \P\l\u\s\长\期\订\阅\
+
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoNoted.js
+
+2、打开软件 > 左上角设置 > 「账户」 或点击「恢复购买项目」
+
+3、解锁成功理论上有消息弹窗，成功后即可关闭脚本。[🚨🚨🚨无效请关掉软件进程后，先打开脚本，再进软件进行解锁]
+
+4、⚠️⚠️⚠️解锁脚本不可共存，请逐一使用并关闭。
+
+
+[mitm]
+hostname = subscription-api.notedapp.io
+
+[rewrite_local]
+https:\/\/subscription-api\.notedapp\.io\/api\/verifyReceipt url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/no.js
+
+********************************/
