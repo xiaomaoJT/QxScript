4d32bd8851b90de9904dd7e19250ba032d390f2a
XiaoMao <895250757@qq.com>
2023-11-06 15:06:41 +0800

    add pko.js

---
+++ rewrite/boxJS/XiaoMaoProKnockOut.js
@@ -0,0 +1,35 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-11-06
+ *
+ * 
+
+
+\P\r\o\K\n\o\c\k\O\u\t\长\期\会\员\订\阅\
+
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoProKnockOut.js
+
+2、打开软件 > 自动解锁 / 不成功结束进程先开脚本再开软件，或重装
+
+
+
+[mitm]
+hostname = buy.itunes.apple.com
+
+[rewrite_local]
+
+https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/pko.js
+
+********************************/
