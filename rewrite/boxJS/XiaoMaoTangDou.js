bbfd6e2199e74118dad0b9f1db2fe56c2ce891f7
XiaoMao <895250757@qq.com>
2023-11-13 11:47:43 +0800

    add td

---
+++ rewrite/boxJS/XiaoMaoTangDou.js
@@ -0,0 +1,33 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-11-13
+ *
+ * 
+
+
+\糖\豆\视\频\ \会\员\解\锁\ \付\费\视\频\解\锁\ \会\员\视\频\解\锁\
+
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · Tg频道频道：https://t.me/xiaomaoJT
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoTangDou.js
+
+
+
+[mitm]
+hostname = aa.tangdou.com,api-h5.tangdou.com
+
+[rewrite_local]
+https:\/\/aa\.tangdou\.com:12308 url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/td.js
+https:\/\/api-h5\.tangdou\.com url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/td.js
+
+********************************/
