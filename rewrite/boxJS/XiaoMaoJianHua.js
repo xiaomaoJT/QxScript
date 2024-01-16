88b578a62bf5325151fc4466180fde5b79f82bac
XiaoMao <895250757@qq.com>
2024-01-16 14:09:58 +0800

    add jh

---
+++ rewrite/boxJS/XiaoMaoJianHua.js
@@ -0,0 +1,34 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2024-01-16
+ *
+ * 
+
+
+\剪\画\ \无\限\时\长
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · TG通知频道：https://t.me/xiaomaoJT
+# XiaoMao · Tg脚本频道：https://t.me/XiaoMaoScript
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoJianHua.js
+
+********************************
+
+[mitm]
+hostname = stream.lestream.cn
+
+[rewrite_local]
+https:\/\/stream\.lestream\.cn\/open\/(customer|mobile)\/(get|setting) url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/jh.js
+
+
+
+ ***************/
