bedfac7667cca6aa28aa190e6ffd8fc1073b9d61
XiaoMao <895250757@qq.com>
2024-01-19 16:16:42 +0800

    update gkld

--- rewrite/boxJS/XiaoMaoGKLD.js
+++ rewrite/boxJS/XiaoMaoGKLD.js
@@ -1,6 +1,6 @@
 /**************************
  *  * @Author: XiaoMao
- * @LastMod: 2023-09-15
+ * @LastMod: 2024-01-19
  *
 
 
@@ -26,16 +26,18 @@ https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoG
 hostname = errlog.umeng.com, api.gongkaoleida.com
 
 [rewrite_local]
-https:\/\/api\.gongkaoleida\.com\/api\/v5_4_4\/exam\/jobSearchList url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
-https:\/\/api\.gongkaoleida\.com\/api\/v5_4_0\/exam\/getExamList url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/exam\/jobSearchList url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/exam\/getExamList url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
 https:\/\/errlog\.umeng\.com\/api\/crashsdk\/logcollect url reject
-https:\/\/api\.gongkaoleida\.com\/api\/v5\/my\/index url reject
-https:\/\/api\.gongkaoleida\.com\/api\/v5_4_3\/sets\/getFilterResult url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
-https:\/\/api\.gongkaoleida\.com\/api\/v4_1_2\/sets\/getAllTips url reject
-https:\/\/api\.gongkaoleida\.com\/api\/v2\/ad\/info url reject
-https:\/\/api\.gongkaoleida\.com\/api\/v5_4_3\/my\/vipCardInfo url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
-https:\/\/api\.gongkaoleida\.com\/api\/v5_3_7\/user\/getInfo url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/my\/index url reject
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/sets\/getFilterResult url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/sets\/getAllTips url reject
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/ad\/info url reject
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/my\/vipCardInfo url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/user\/getInfo url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
 https:\/\/member\.aliyundrive\.com\/v1\/config\/customCapsule url reject-dict
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/exam\/jobDetail url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v.+\/subscribe\/jobHot url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
 
  ***************/
 
