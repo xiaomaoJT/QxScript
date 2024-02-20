20601c46535dfaa1a38c9d17cd6c22da5c4a41d3
XiaoMao <895250757@qq.com>
2024-02-20 09:23:37 +0800

    add yo & gzMetro

--- UPDATELOG.md
+++ UPDATELOG.md
@@ -66,11 +66,18 @@ resource_parser_url= https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/mast
 
 > 小帽语：日常更新如**节点订阅更新**、**去广告分流更新**、**去广告重写更新**将不推送更新提示及更新日志，请自行QX软件内开启对应资源的默认更新即可自动获取，最新更新时间以首页或对应资源内说明文字或本页的更新时间为准。
 >
-> 本仓自3.0版本后 于2022年12月1日起 已持续更新 ***555*** 次
+> 本仓自3.0版本后 于2022年12月1日起 已持续更新 ***556*** 次
 >
-> ***最新更新时间 2024.02.04 17:20***
+> ***最新更新时间 2024.02.20 09:30***
 
 
+
++ ##### 20240220
+
+  * > 新增[XiaoMaoGuangZhouMetro脚本](https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoGuangZhouMetro.js)
+    >
+    > 新增[yo脚本](https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoYosum.js)
+    >
 
 + ##### 20240129
 
---
+++ rewrite/boxJS/XiaoMaoGuangZhouMetro.js
@@ -0,0 +1,171 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2024-02-19
+ *
+ * 
+
+
+\广\州\地\铁\实\时\信\息\查\询\
+
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · TG通知频道：https://t.me/xiaomaoJT
+# XiaoMao · Tg脚本频道：https://t.me/XiaoMaoScript
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+
+⚠️ 配置文件 [task_local] 标签添加
+
+0 0 9 * * ? https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoGuangZhouMetro.js, tag=🚇XiaoMao_广州地铁实时信息查询, img-url=tram.fill.system, enabled=true
+
+单独脚本地址：
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoGuangZhouMetro.js
+
+********************************/
+
+const $ = new Env("XiaoMaoMetro");
+let url = "https://ydyc.gzmtr.cn:19090/interface/info?equipmentType=pc";
+let option = {
+  url: encodeURI(url),
+  method: "GET",
+};
+
+$.get(option, (err, resp, response) => {
+  if (response) {
+    let responseJson = JSON.parse(response);
+    let returnText = "";
+
+    if (responseJson.data.msg.ch != "" && responseJson.data.msg.en != "") {
+      returnText =
+        responseJson.data.msg.ch.replace(/<[^>]+>/g, '') +
+        "\n" +
+        responseJson.data.msg.en.replace(/<[^>]+>/g, '');
+    } else {
+      returnText = "当前全线网通畅，运营服务正常！";
+    }
+
+    $.notify(
+      "🚇广州地铁实时信息查询",
+      "更新时间「" + responseJson.data.upToData + "」",
+      returnText,
+      ""
+    );
+  } else {
+    getError();
+  }
+});
+
+function getError() {
+  $.notify(
+    "🚇XiaoMao_广州地铁实时信息查询失败❗️",
+    "",
+    "🚧请稍后再试❗️",
+    "https://i.pixiv.re/img-original/img/2020/10/14/16/34/51/85008145_p0.jpg"
+  );
+}
+setTimeout(() => {
+  $done({});
+}, 2000);
+
+function Env(name) {
+  // 判断当前环境是否为 Loon
+  const isLoon = typeof $loon !== "undefined";
+  // 判断当前环境是否为 Surge
+  const isSurge = typeof $httpClient !== "undefined" && !isLoon;
+  // 判断当前环境是否为 QuantumultX
+  const isQX = typeof $task !== "undefined";
+
+  // 定义 read 方法，用于读取数据
+  const read = (key) => {
+    if (isLoon || isSurge) return $persistentStore.read(key);
+    if (isQX) return $prefs.valueForKey(key);
+  };
+
+  // 定义 write 方法，用于写入数据
+  const write = (key, value) => {
+    if (isLoon || isSurge) return $persistentStore.write(key, value);
+    if (isQX) return $prefs.setValueForKey(key, value);
+  };
+
+  // 定义 notify 方法，用于发送通知
+  const notify = (
+    title = "XiaoMao",
+    subtitle = "",
+    message = "",
+    url = "",
+    url2 = url
+  ) => {
+    if (isLoon) $notification.post(title, subtitle, message, url);
+    if (isSurge) $notification.post(title, subtitle, message, { url });
+    if (isQX)
+      $notify(title, subtitle, message, { "open-url": url, "media-url": url2 });
+  };
+
+  // 定义 get 方法，用于发送 GET 请求
+  const get = (url, callback) => {
+    if (isLoon || isSurge) $httpClient.get(url, callback);
+    if (isQX) {
+      url.method = `GET`;
+      $task.fetch(url).then((resp) => callback(null, {}, resp.body));
+    }
+  };
+
+  // 定义 post 方法，用于发送 POST 请求
+  const post = (url, callback) => {
+    if (isLoon || isSurge) $httpClient.post(url, callback);
+    if (isQX) {
+      url.method = `POST`;
+      $task.fetch(url).then((resp) => callback(null, {}, resp.body));
+    }
+  };
+
+  // 定义 put 方法，用于发送 PUT 请求
+  const put = (url, callback) => {
+    if (isLoon || isSurge) $httpClient.put(url, callback);
+    if (isQX) {
+      url.method = "PUT";
+      $task.fetch(url).then((resp) => callback(null, {}, resp.body));
+    }
+  };
+
+  // 定义 toObj 方法，用于将字符串转为对象
+  const toObj = (str) => JSON.parse(str);
+
+  // 定义 toStr 方法，用于将对象转为字符串
+  const toStr = (obj) => JSON.stringify(obj);
+
+  // 定义 queryStr 方法，用于将对象转为可以请求的字符串
+  const queryStr = (obj) => {
+    return Object.keys(obj)
+      .map((key) => `${key}=${obj[key]}`)
+      .join("&");
+  };
+
+  // 定义 log 方法，用于输出日志
+  const log = (message) => console.log(message);
+
+  // 定义 done 方法，用于结束任务
+  const done = (value = {}) => $done(value);
+
+  // 返回包含所有方法的对象
+  return {
+    name,
+    read,
+    write,
+    notify,
+    get,
+    post,
+    put,
+    toObj,
+    toStr,
+    queryStr,
+    log,
+    done,
+  };
+}
