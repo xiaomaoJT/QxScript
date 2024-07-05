39b4c9290aa5e95ba7f25224cf2a1172001adccf
XiaoMao <895250757@qq.com>
2024-07-05 15:24:16 +0800

    update

--- rewrite/boxJS/source/gkld.js
+++ rewrite/boxJS/source/gkld.js
@@ -1,375 +1,125 @@
-var encode_version = "jsjiami.com.v5",
-  duwmw = "__0x10e30f",
-  __0x10e30f = [
-    "wpkjYsKvw7w=",
-    "w5fDnh3DpA==",
-    "dMKrw4ZBwoUJw44=",
-    "Fz/Cp1nCqgDCkcKWwrJ4",
-    "wpNgT8OXDMK+M25WwrLDoMKq",
-    "wpQlwr7CmcOWT8OY",
-    "LcOVXF4=",
-    "Gn/DqMK2w5XCin15w5fCsQtDFQ==",
-    "NWfDgcKvw4c=",
-    "w4DDghzDr8KFwqUrw6M=",
-    "w4oaHSEVUsKeP8OiwoTCtwY2GcKo",
-    "WU3DqxA=",
-    "w5nCtcORFcKSw67Cj2c=",
-    "w4d/VsOgOcKtNm9/w7nCqcOv",
-    "6bqK6YaAPMKXw5o=",
-    "5pmR6ZOqPMKXw5o=",
-    "W8Oqw7LDqQ==",
-    "PRfDt8OF",
-    "wpMzwqvCh8O+QsObfg==",
-    "woHCpMOhwrfDh8KYwrha",
-    "K8K8w68J",
-    "w4rDvcKaRiJfw53Cmw==",
-    "KMOlwpZow7IAGl4=",
-    "w5HDpkfCgA==",
-    "P8OXwqwAwqIcwpto",
-    "UTfCoHk=",
-    "w4DDtFbCk8OtHQfClQ==",
-    "VCJPN3oUVQ7CukjCqMKBEkg=",
-    "wqDDgcK3PQ==",
-    "wqTDqHXDqQ==",
-    "w4bCqcOWNMKvw6HCj24=",
-    "w4PDrkPCpsOWEgXCnw==",
-    "wrzCvcKMBw==",
-    "LB52w4jDhxw8wr8Ewr10w5M=",
-    "w7DCkW7DgAfDucObHg==",
-    "asO5dCE=",
-    "JcOKwpbCrg==",
-    "d8OvUTTCkcKVw6o=",
-    "wrFaUxnDghY=",
-    "w5HCtMOUw7cGw5k=",
-    "SmLCr8Kr",
-    "ScOiw7bDiXvCh0zChyh2Kw==",
-    "T3HCr8KjwoTCmi1HwojCrFVFTG7DusOkwqg9",
-    "w5jCo8OHEw==",
-    "KjAewr8=",
-    "w5zDtGXCgMOIGgU=",
-    "A8Klw7vCtQ==",
-    "w6/CgcKDw5jDpMO0",
-    "wpdvw6TCvGbDrg==",
-    "w54sw7RDFwApNMOVWEk=",
-    "ecKLJ8KG",
-    "wovCrMO2wr0=",
-    "6LWq57mq5pqd6ZGn5L6z5ZKh",
-    "O8OEQX7ChHNF",
-    "5pqz6ZG9w6Iuwos=",
-    "wqdeVA0=",
-    "w5thGcKhwqbDscO4b8Orw6M=",
-    "w6nDogF6w7DCkjbCv18cJw==",
-    "woAQwp4RdMOIw67CvsK5",
-    "woIhwrrClA==",
-    "N8OCwpLChhMYYA==",
-    "cMKnw4ZqwpYLw4/CiA==",
-    "wpJyw7XCjWvDrcKeMMOd",
-    "w7vDqgVa",
-    "aMO1dxzCk8Kaw6E=",
-    "JsKuw5odwq05",
-    "JsKuw40Bwqk=",
-    "LMOeS0/Ch2JfwqrCpg==",
-    "54uw5p2T5Y6J772CEkzkv6Hlr4vmnpnlv67nqZTvvavovprorpvml7TmjI/mi4nkuYPnm43ltpHkv48=",
-    "IT3DhcO8Uw==",
-    "w6zCi3TDrhTDtcOWVTXCux1IwpZQ",
-    "w6xgF8KTwr4=",
-    "WsO/w4jDnMOP",
-  ];
-(function (_0x9b5140, _0x374b86) {
-  var _0x5c8a95 = function (_0x2af1df) {
-    while (--_0x2af1df) {
-      _0x9b5140["push"](_0x9b5140["shift"]());
-    }
-  };
-  _0x5c8a95(++_0x374b86);
-})(__0x10e30f, 0x16d);
-var _0x28bf = function (_0x270e40, _0x5af56d) {
-  _0x270e40 = _0x270e40 - 0x0;
-  var _0xd2ebaf = __0x10e30f[_0x270e40];
-  if (_0x28bf["initialized"] === undefined) {
-    (function () {
-      var _0xf802e5 =
-        typeof window !== "undefined"
-          ? window
-          : typeof process === "object" &&
-            typeof require === "function" &&
-            typeof global === "object"
-          ? global
-          : this;
-      var _0x3b9d =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-      _0xf802e5["atob"] ||
-        (_0xf802e5["atob"] = function (_0x1b1b5a) {
-          var _0x257059 = String(_0x1b1b5a)["replace"](/=+$/, "");
-          for (
-            var _0x34e5f5 = 0x0,
-              _0x647df1,
-              _0x2aff83,
-              _0x26de26 = 0x0,
-              _0xb121d = "";
-            (_0x2aff83 = _0x257059["charAt"](_0x26de26++));
-            ~_0x2aff83 &&
-            ((_0x647df1 =
-              _0x34e5f5 % 0x4 ? _0x647df1 * 0x40 + _0x2aff83 : _0x2aff83),
-            _0x34e5f5++ % 0x4)
-              ? (_0xb121d += String["fromCharCode"](
-                  0xff & (_0x647df1 >> ((-0x2 * _0x34e5f5) & 0x6))
-                ))
-              : 0x0
-          ) {
-            _0x2aff83 = _0x3b9d["indexOf"](_0x2aff83);
-          }
-          return _0xb121d;
-        });
-    })();
-    var _0x28217d = function (_0x2ce947, _0x393b15) {
-      var _0x3f0a71 = [],
-        _0xf08cc = 0x0,
-        _0x524aa0,
-        _0x4e3fa0 = "",
-        _0x2b5a80 = "";
-      _0x2ce947 = atob(_0x2ce947);
-      for (
-        var _0x36527f = 0x0, _0x2b9575 = _0x2ce947["length"];
-        _0x36527f < _0x2b9575;
-        _0x36527f++
-      ) {
-        _0x2b5a80 +=
-          "%" +
-          ("00" + _0x2ce947["charCodeAt"](_0x36527f)["toString"](0x10))[
-            "slice"
-          ](-0x2);
-      }
-      _0x2ce947 = decodeURIComponent(_0x2b5a80);
-      for (var _0x50277 = 0x0; _0x50277 < 0x100; _0x50277++) {
-        _0x3f0a71[_0x50277] = _0x50277;
-      }
-      for (_0x50277 = 0x0; _0x50277 < 0x100; _0x50277++) {
-        _0xf08cc =
-          (_0xf08cc +
-            _0x3f0a71[_0x50277] +
-            _0x393b15["charCodeAt"](_0x50277 % _0x393b15["length"])) %
-          0x100;
-        _0x524aa0 = _0x3f0a71[_0x50277];
-        _0x3f0a71[_0x50277] = _0x3f0a71[_0xf08cc];
-        _0x3f0a71[_0xf08cc] = _0x524aa0;
-      }
-      _0x50277 = 0x0;
-      _0xf08cc = 0x0;
-      for (var _0x5d961a = 0x0; _0x5d961a < _0x2ce947["length"]; _0x5d961a++) {
-        _0x50277 = (_0x50277 + 0x1) % 0x100;
-        _0xf08cc = (_0xf08cc + _0x3f0a71[_0x50277]) % 0x100;
-        _0x524aa0 = _0x3f0a71[_0x50277];
-        _0x3f0a71[_0x50277] = _0x3f0a71[_0xf08cc];
-        _0x3f0a71[_0xf08cc] = _0x524aa0;
-        _0x4e3fa0 += String["fromCharCode"](
-          _0x2ce947["charCodeAt"](_0x5d961a) ^
-            _0x3f0a71[(_0x3f0a71[_0x50277] + _0x3f0a71[_0xf08cc]) % 0x100]
-        );
-      }
-      return _0x4e3fa0;
-    };
-    _0x28bf["rc4"] = _0x28217d;
-    _0x28bf["data"] = {};
-    _0x28bf["initialized"] = !![];
-  }
-  var _0xeab70d = _0x28bf["data"][_0x270e40];
-  if (_0xeab70d === undefined) {
-    if (_0x28bf["once"] === undefined) {
-      _0x28bf["once"] = !![];
-    }
-    _0xd2ebaf = _0x28bf["rc4"](_0xd2ebaf, _0x5af56d);
-    _0x28bf["data"][_0x270e40] = _0xd2ebaf;
-  } else {
-    _0xd2ebaf = _0xeab70d;
-  }
-  return _0xd2ebaf;
-};
-let requestUrl = $request["url"];
-let obj = JSON[_0x28bf("0x0", "S8Xb")](
-  $response[_0x28bf("0x1", "f@1@")]
-    ["replace"](/\"isAuth\":false/g, "\x22isAuth\x22:true")
-    [_0x28bf("0x2", "4xSm")](/\"isAuth\":\"0\"/g, _0x28bf("0x3", "TZ#o"))
-    ["replace"](/\"isVip\":\"0\"/g, "\x22isVip\x22:1")
-    ["replace"](/\"vipGrade\":\"0\"/g, _0x28bf("0x4", "!fO("))
-    [_0x28bf("0x5", "mx0$")](
-      /\"vipExpire\":\"0\"/g,
-      "\x22vipExpire\x22:1726411565000"
+let status = isJSON($response.body);
+let requestUrl = $request.url;
+var obj = status
+  ? JSON.parse(
+      removeExtraSpaces($response.body)
+        .replace(/\"isAuth\":false/g, '"isAuth":true')
+        .replace(/\"isValid\":\w+/g, '"isValid":1')
+        .replace(/\"isValid\":\"0\"/g, '"isValid":1')
+        .replace(/\"isAuth\":\w+/g, '"isAuth":1')
+        .replace(/\"isAuth\":\"0\"/g, '"isAuth":1')
+        .replace(/\"isVip\":\w+/g, '"isVip":1')
+        .replace(/\"isVip\":\"0\"/g, '"isVip":1')
+        .replace(/\"vipGrade\":\w+/g, '"vipGrade":2')
+        .replace(/\"vipGrade\":\"0\"/g, '"vipGrade":2')
+        .replace(/\"vipExpire\":\w+/g, '"vipExpire":1726411565000')
+        .replace(/\"vipExpire\":\"0\"/g, '"vipExpire":1726411565000')
+        .replace(/\"jobBianzhiOpt\":0/g, '"jobBianzhiOpt":1')
+        .replace(/\"status\":\w+/g, '"status":1')
     )
-);
+  : $response.body;
 if (
-  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/user\/getInfo?/[
-    _0x28bf("0x6", "jJe#")
-  ](requestUrl)
+  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/user\/getInfo?/.test(requestUrl)
 ) {
-  var _0x1a562f = _0x28bf("0x7", "7Upz")[_0x28bf("0x8", "l$tp")]("|"),
-    _0x48328e = 0x0;
-  while (!![]) {
-    switch (_0x1a562f[_0x48328e++]) {
-      case "0":
-        obj["data"][_0x28bf("0x9", "f@1@")][_0x28bf("0xa", "%82S")] = 0x0;
-        continue;
-      case "1":
-        obj[_0x28bf("0xb", "i][4")][_0x28bf("0xc", "k]FP")][
-          _0x28bf("0xd", "!fO(")
-        ] = [
-          {
-            vipExpire: 0x191f625e7c8,
-            vipGradeName: _0x28bf("0xe", "ZoQ2"),
-            vipGrade: 0x1,
-            remainDays: 0x16d,
-            isVip: 0x1,
-          },
-          {
-            vipExpire: 0x191f625e7c8,
-            vipGradeName: _0x28bf("0xf", "ZoQ2"),
-            vipGrade: 0x2,
-            remainDays: 0x16d,
-            isVip: 0x1,
-          },
-        ];
-        continue;
-      case "2":
-        obj[_0x28bf("0x10", "Jza0")]["userInfo"]["vipExpire"] = 0x191f625e7c8;
-        continue;
-      case "3":
-        obj[_0x28bf("0x11", "bSIl")]["userInfo"]["vipGrade"] = 0x2;
-        continue;
-      case "4":
-        obj["data"][_0x28bf("0x12", "mx0$")][_0x28bf("0x13", "4n@2")] =
-          obj[_0x28bf("0x14", "cqUi")][_0x28bf("0x15", "mCGB")][
-            _0x28bf("0x16", "ID(J")
-          ] + "_xiaomao";
-        continue;
-      case "5":
-        obj[_0x28bf("0x17", "w&dE")][_0x28bf("0x18", "r!m2")]["isVip"] = 0x1;
-        continue;
-      case "6":
-        obj[_0x28bf("0x19", "TZ#o")][_0x28bf("0x1a", "w&dE")][
-          _0x28bf("0x1b", "V*@o")
-        ] = 0x0;
-        continue;
-    }
-    break;
-  }
-} else if (
-  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/sets\/getFilterResult?/[
-    _0x28bf("0x1c", "*l9(")
-  ](requestUrl)
-) {
-  obj[_0x28bf("0x1d", "EvSy")][_0x28bf("0x1e", "k]FP")][
-    _0x28bf("0x1f", "w&dE")
-  ] = 0x0;
-  obj[_0x28bf("0x20", "rsAm")][_0x28bf("0x21", "!kk2")][
-    _0x28bf("0x22", "WcNd")
-  ] = 0x0;
-} else if (
-  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/exam\/getExamList?/[
-    _0x28bf("0x23", "2$bc")
-  ](requestUrl)
-) {
-  obj[_0x28bf("0x24", "3B0d")][_0x28bf("0x25", "2$bc")] = 0x0;
-  obj["data"][_0x28bf("0x26", "ygGI")][_0x28bf("0x27", "!x%V")] = 0xc8;
-  obj[_0x28bf("0x28", "7Upz")][_0x28bf("0x29", "Jza0")][
-    _0x28bf("0x2a", "7Upz")
-  ] = 0x1;
-} else if (
-  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/exam\/jobSearchList?/[
-    _0x28bf("0x2b", "k]FP")
-  ](requestUrl)
-) {
-  obj[_0x28bf("0x2c", "ZoQ2")][_0x28bf("0x2d", "w&dE")] = 0x0;
-  obj[_0x28bf("0x2e", "A&r*")][_0x28bf("0x2f", "sJBn")][
-    _0x28bf("0x30", "Go0t")
-  ] = 0xc8;
-  obj[_0x28bf("0x1d", "EvSy")][_0x28bf("0x31", "bc1E")]["jobBianzhiOpt"] = 0x1;
-} else if (
-  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/my\/vipCardInfo?/[
-    _0x28bf("0x32", "C9r7")
-  ](requestUrl)
-) {
-  obj[_0x28bf("0x33", "4n@2")]["btnScheme"] = "";
-  obj["data"]["greetingMsg"] = _0x28bf("0x34", "4n@2");
-  obj["data"][_0x28bf("0x35", "jJe#")] = _0x28bf("0x36", "k]FP");
-  obj[_0x28bf("0x37", "ygGI")][_0x28bf("0x38", "VxDb")] = [];
-} else if (
-  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/exam\/jobDetail?/["test"](
-    requestUrl
-  )
-) {
-  obj[_0x28bf("0x28", "7Upz")][_0x28bf("0x39", "3*I4")] = {
-    historyCompetitionData: 0x1,
-    jobEnrollForecast: 0x1,
-    historyInterviewScoreInfo: 0x1,
-  };
-} else if (
-  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/subscribe\/jobHot?/["test"](
-    requestUrl
-  )
-) {
-  obj["data"]["isVip"] = 0x1;
-  obj["data"]["hasUnlock"] = !![];
-} else if (
-  /^https:\/\/api\.gongkaoleida\.com\/api\/v5_2_6\/jobHistory\/getJobHistory?/[
-    "test"
-  ](requestUrl)
-) {
-  var _0x4acb51 = _0x28bf("0x3a", "OL]c")["split"]("|"),
-    _0x2195d9 = 0x0;
-  while (!![]) {
-    switch (_0x4acb51[_0x2195d9++]) {
-      case "0":
-        obj[_0x28bf("0x3b", "mx0$")][_0x28bf("0x3c", "3B0d")][
-          _0x28bf("0x3d", "4xSm")
-        ] = 0x1;
-        continue;
-      case "1":
-        obj["data"]["vipInfo"][_0x28bf("0x3e", "Go0t")] = 0x1;
-        continue;
-      case "2":
-        obj[_0x28bf("0x3f", "3*I4")][_0x28bf("0x40", "2$bc")][
-          "vipExpireDate"
-        ] = 0x191f625e7c8;
-        continue;
-      case "3":
-        obj["data"][_0x28bf("0x41", "cqUi")] = !![];
-        continue;
-      case "4":
-        obj["data"]["vipInfo"][_0x28bf("0x42", "cqUi")] = 0x1;
-        continue;
-    }
-    break;
-  }
-}
-$done({ body: JSON["stringify"](obj) });
-(function (_0x4ea26b, _0x3279bf, _0x5ddf78) {
-  var _0x4aef80 = {
-    xKFXU: _0x28bf("0x43", "jJe#"),
-    UnzFw: function _0x119cdc(_0xb03649, _0x10bb90) {
-      return _0xb03649 + _0x10bb90;
+  obj.data.userInfo.nickname = obj.data.userInfo.nickname;
+  obj.data.userInfo.vipGrade = 2;
+  obj.data.userInfo.vipExpire = 1726411565000;
+  obj.data.userInfo.isVip = 1;
+  obj.data.userInfo.homePageStatus = 0;
+  obj.data.userInfo.recommendStatus = 0;
+  obj.data.userInfo.vipGradeList = [
+    {
+      vipExpire: 1726411565000,
+      vipGradeName: "黄金VIP",
+      vipGrade: 1,
+      remainDays: 365,
+      isVip: 1,
     },
-    fweIp: _0x28bf("0x44", "%82S"),
-    jaApD: "删除版本号，js会定期弹窗",
+    {
+      vipExpire: 1726411565000,
+      vipGradeName: "星钻VIP",
+      vipGrade: 2,
+      remainDays: 365,
+      isVip: 1,
+    },
+  ];
+} else if (
+  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/sets\/getFilterResult?/.test(
+    requestUrl
+  )
+) {
+  obj.data.jobStaff.vipGrade = 2;
+  obj.data.articleStaff.vipGrade = 2;
+} else if (
+  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/exam\/getExamList?/.test(
+    requestUrl
+  )
+) {
+  obj.data.isValid = 1;
+  obj.data.result.status = 200;
+  obj.data.vipAuthList.articleBianzhiSort = 1;
+} else if (
+  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/exam\/jobSearchList?/.test(
+    requestUrl
+  )
+) {
+  obj.data.isValid = 1;
+  obj.data.result.status = 200;
+  obj.data.vipAuthList.jobBianzhiOpt = 1;
+} else if (
+  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/my\/vipCardInfo?/.test(
+    requestUrl
+  )
+) {
+  obj.data.btnScheme = "";
+  obj.data.greetingMsg = "超级星钻会员";
+  obj.data.btnText = "星钻VIP";
+  obj.data.bottomList = [];
+} else if (
+  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/exam\/jobDetail?/.test(
+    requestUrl
+  )
+) {
+  obj.data.vipAuthList = {
+    historyCompetitionData: 1,
+    jobEnrollForecast: 1,
+    historyInterviewScoreInfo: 1,
   };
-  _0x5ddf78 = "al";
+} else if (
+  /^https:\/\/api\.gongkaoleida\.com\/api\/v.+\/subscribe\/jobHot?/.test(
+    requestUrl
+  )
+) {
+  obj.data.isVip = 1;
+  obj.data.hasUnlock = true;
+} else if (
+  /^https:\/\/api\.gongkaoleida\.com\/api\/v5_2_6\/jobHistory\/getJobHistory?/.test(
+    requestUrl
+  )
+) {
+  obj.data.vipInfo.vipExpireDate = 1726411565000;
+  obj.data.vipInfo.vipExpire = 1;
+  obj.data.vipInfo.isVip = 1;
+  obj.data.vipInfo.vipGrade = 2;
+  obj.data.isAuth = true;
+}
+$done({ body: status ? JSON.stringify(obj) : obj });
+
+function isJSON(str) {
   try {
-    _0x5ddf78 += "ert";
-    _0x3279bf = encode_version;
-    if (
-      !(
-        typeof _0x3279bf !== _0x4aef80[_0x28bf("0x45", "bSIl")] &&
-        _0x3279bf === _0x28bf("0x46", "WcNd")
-      )
-    ) {
-      _0x4ea26b[_0x5ddf78](
-        _0x4aef80[_0x28bf("0x47", "VxDb")](
-          "删除",
-          _0x4aef80[_0x28bf("0x48", "Ieg7")]
-        )
-      );
-    }
-  } catch (_0x41a70e) {
-    _0x4ea26b[_0x5ddf78](_0x4aef80["jaApD"]);
+    JSON.parse(str);
+  } catch (e) {
+    return false;
   }
-})(window);
-encode_version = "jsjiami.com.v5";
+  return true;
+}
+
+function removeExtraSpaces(jsonString) {
+  var jsonObj = JSON.parse(jsonString);
+  return JSON.stringify(jsonObj, function (key, value) {
+    if (typeof value === "string") {
+      return value.trim();
+    }
+    return value;
+  });
+}
