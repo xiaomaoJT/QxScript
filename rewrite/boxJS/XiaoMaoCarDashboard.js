a34224dc3ea8c1f699db2f94e1f01329ce4dae86
XiaoMao <895250757@qq.com>
2024-10-16 16:41:00 +0800

    add cd \ shm

---
+++ rewrite/boxJS/XiaoMaoCarDashboard.js
@@ -0,0 +1,36 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2024-10-11
+ *
+ * 
+
+
+\C\a\r\D\a\s\h\b\o\a\r\d\ \p\r\o\
+
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · TG通知频道：https://t.me/xiaomaoJT
+# XiaoMao · Tg脚本频道：https://t.me/XiaoMaoScript
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoCarDashboard.js
+
+2、打开软件 > 自动解锁 ，若未解锁成功，手动点击「恢复购买」
+
+3、⚠️⚠️⚠️同类型解锁脚本不可共存。
+
+[mitm]
+hostname = buy.itunes.apple.com
+
+[rewrite_local]
+
+https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/cd.js
+
+********************************/
