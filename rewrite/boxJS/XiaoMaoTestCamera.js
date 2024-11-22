51be49301161622c65002c343e4a452a2710eb8f
XiaoMao <895250757@qq.com>
2024-11-22 09:59:44 +0800

    update XiaoMaoTestCamera

--- rewrite/boxJS/XiaoMaoTestCamera.js
+++ rewrite/boxJS/XiaoMaoTestCamera.js
@@ -21,7 +21,7 @@
 
 使用方法：
 1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
-https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaTestCamera.js
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoTestCamera.js
 
 2、打开软件 > 设置 > 点击「恢复购买」
 
