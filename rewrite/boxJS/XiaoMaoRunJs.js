c7468fee17bb256e37172b144549213ad269c550
XiaoMao <895250757@qq.com>
2024-12-12 15:43:22 +0800

    add runjs

---
+++ rewrite/boxJS/XiaoMaoRunJs.js
@@ -0,0 +1,68 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2024-12-12
+ *
+ * 
+
+
+\R\u\n\J\S\ \P\r\o\
+
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · TG通知频道：https://t.me/xiaomaoJT
+# XiaoMao · Tg脚本频道：https://t.me/XiaoMaoScript
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoRunJs.js
+
+
+
+[mitm]
+hostname = runjs.work
+
+[rewrite_local]
+https:\/\/runjs\.work\/transformer\/users\/me url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/XiaoMaoRunJs.js
+
+********************************/
+
+
+let status = isJSON($response.body);
+let requestUrl = $request.url;
+var obj = status
+  ? JSON.parse(
+      removeExtraSpaces($response.body)
+        .replace(/\"isPro\":0/g, '"isPro":1')
+        .replace(/\"isMobanPro\":0/g, '"isMobanPro":1')
+    )
+  : $response.body;
+
+$done({ body: status ? JSON.stringify(obj) : obj });
+
+function isJSON(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+function removeExtraSpaces(jsonString) {
+  var jsonObj = JSON.parse(jsonString);
+  return JSON.stringify(jsonObj, function (key, value) {
+    if (typeof value === "string") {
+      return value.trim();
+    }
+    return value;
+  });
+}
+
+
+
