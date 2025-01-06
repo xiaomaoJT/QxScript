00c802286193ed1ee86bc4a17d179a5ae3e32b22
XiaoMao <895250757@qq.com>
2025-01-06 16:41:21 +0800

    add dm / ftr / vis

---
+++ rewrite/boxJS/XiaoMaoDaMa.js
@@ -0,0 +1,34 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2024-12-26
+ *
+ * 
+
+
+\沉\浸\式\打\码\
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · TG通知频道：https://t.me/xiaomaoJT
+# XiaoMao · Tg脚本频道：https://t.me/XiaoMaoScript
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoDaMa.js
+
+2、打开软件 > 右下角角按钮 > 底部中间购物车按钮 > 自动解锁年度会员 ，若未解锁成功，手动点击「恢复购买」
+
+
+[mitm]
+hostname = blurid-verify.dolores.workers.dev
+
+[rewrite_local]
+
+https:\/\/blurid-verify\.dolores\.workers\.dev url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/dm.js
+
+********************************/
